8cda6c113fe62a2dd7cb79dfe6078281720d3c0e
daniel corcoran <danielc.92@outlook.com>
2025-04-05 14:40:58 +1100

    feat: PricingStrategy logic and tests

--- src/PricingStrategy/__tests__/index.spec.ts
+++ src/PricingStrategy/__tests__/index.spec.ts
@@ -1,1 +1,172 @@
-describe("PricingStrategy test suite", () => {})
+/*
+    critical paths to test;
+
+    1. SecondBite
+    - Gets a 3 for 2 deal on Classic Ads
+    2. Axil Coffee Roasters
+    - Gets a discount on Stand out Ads where the price drops to $299.99 per ad
+    3. MYER
+    - Gets a 5 for 4 deal on Stand out Ads
+    - Gets a discount on Premium Ads where the price drops to $389.99 per ad
+
+*/
+
+import { FixedDiscountPricingStrategy, GroupBuyPricingStrategy } from ".."
+import { AdNameEnum, AllAds } from "../../Ads"
+
+describe("PricingStrategy test suite", () => {
+    describe("FixedDiscountPricingStrategy", () =>{
+
+        it("should throw error fixedDiscountAmount is greater than the value of the ad", () =>{
+            const strategy = new FixedDiscountPricingStrategy(AdNameEnum.PREMIUM, {
+                fixedDiscountAmount: 999_999_999,
+            })
+            expect(
+               () => strategy.calculatePrice(1)
+            ).toThrow("fixedDiscountAmount must be less than the value of the ad: PREMIUM")
+        })
+
+        it("should throw error fixedDiscountAmount is less than 0", () =>{
+            const strategy = new FixedDiscountPricingStrategy(AdNameEnum.PREMIUM, {
+                fixedDiscountAmount: -10,
+            })
+            expect(
+               () => strategy.calculatePrice(5)
+            ).toThrow("fixedDiscountAmount must be a positive number")
+        })
+
+        it("should throw error when numbers are not supplied to calculatePrice()", () =>{
+            const strategy = new FixedDiscountPricingStrategy(AdNameEnum.PREMIUM, {
+                fixedDiscountAmount: undefined,
+            })
+            expect(
+               () => strategy.calculatePrice(1)
+            ).toThrow("fixedDiscountAmount and totalNumberOfAds must be of type number")
+        })
+
+        it("should calculate a price drop to 389.99 on 1 x premium ads", () =>{
+            const currentAd = AdNameEnum.PREMIUM
+            const currentAdPrice = AllAds[currentAd].retailPriceCentsAUD
+
+            const strategy = new FixedDiscountPricingStrategy(currentAd, {
+                // discount by $5 (500 cents)
+                fixedDiscountAmount: 500,
+               
+            })
+            expect(
+                strategy.calculatePrice(1)
+            ).toEqual(currentAdPrice - 500)
+        })
+
+        it("should calculate a price drop to 1169.97 on 3 x premium ads", () =>{
+            const currentAd = AdNameEnum.PREMIUM
+            const currentAdPrice = AllAds[currentAd].retailPriceCentsAUD
+            const strategy = new FixedDiscountPricingStrategy(currentAd, {
+                // discount by $5 (500 cents)
+                fixedDiscountAmount: 500,
+            
+            })
+            expect(
+                strategy.calculatePrice(3)
+            ).toEqual(3 * (currentAdPrice - 500))
+        })
+    })
+
+    describe("GroupBuyPricingStrategy", () => {
+        it("should throw error when lower or upper limits are undefined", () => {
+            const strategy = new GroupBuyPricingStrategy(AdNameEnum.CLASSIC, {
+                groupBuyLowerLimit: undefined,
+                groupBuyUpperLimit: undefined,
+        
+            })
+            expect(
+                () => strategy.calculatePrice(3)
+            ).toThrow(
+                "groupBuyLowerLimit, groupBuyUpperLimit and totalNumberOfAds must be of type number"
+            )
+        })
+
+        it("should throw error when lower limit is less than 1", () => {
+            const strategy = new GroupBuyPricingStrategy(AdNameEnum.CLASSIC, {
+                groupBuyLowerLimit: 0,
+                groupBuyUpperLimit: 3,
+        
+            })
+            expect(
+                () => strategy.calculatePrice(3)
+            ).toThrow(
+              "the lower limit must be at least 1"
+            )
+        })
+
+        it("should throw error when upper limit is less than 2", () => {
+            const strategy = new GroupBuyPricingStrategy(AdNameEnum.CLASSIC, {
+                groupBuyLowerLimit: 10,
+                groupBuyUpperLimit: 1,
+        
+            })
+            expect(
+                () => strategy.calculatePrice(3)
+            ).toThrow(
+               "the groupBuyUpperLimit must be at least 2"
+            )
+        })
+
+        it("should throw error when upper limit is less than lower limit", () => {
+            const strategy = new GroupBuyPricingStrategy(AdNameEnum.CLASSIC, {
+                groupBuyLowerLimit: 10,
+                groupBuyUpperLimit: 4,
+          
+            })
+            expect(
+                () => strategy.calculatePrice(3)
+            ).toThrow(
+               "the upper limit must be greater than the lower limit"
+            )
+        })
+
+        it("should calculate price for a 3 for 2 deal on CLASSIC ads accurately", () => {
+            const strategy = new GroupBuyPricingStrategy(AdNameEnum.CLASSIC, {
+                groupBuyLowerLimit: 2,
+                groupBuyUpperLimit: 3,
+               
+            })
+            expect(
+                strategy.calculatePrice(3)
+            ).toEqual(
+                AllAds.CLASSIC.retailPriceCentsAUD * 2
+            )
+        })
+
+        it("should calculate price for a 5 for 4 deal on STAND_OUT ads accurately", () => {
+            const strategy = new GroupBuyPricingStrategy(AdNameEnum.STAND_OUT, {
+                groupBuyLowerLimit: 4,
+                groupBuyUpperLimit: 5,
+        
+            })
+            expect(
+                strategy.calculatePrice(5)
+            ).toEqual(
+                AllAds.STAND_OUT.retailPriceCentsAUD * 4
+            )
+        })
+
+        // this is not part of the exercise requirements but tests an edge case when you have remainder units
+        it("should calculate price for a 5 for 4 deal on STAND_OUT ads accurately WITH REMAINDER", () => {
+            const currentAd = AdNameEnum.STAND_OUT
+            const currentAdPrice = AllAds[currentAd].retailPriceCentsAUD
+            const strategy = new GroupBuyPricingStrategy(currentAd, {
+                groupBuyLowerLimit: 4,
+                groupBuyUpperLimit: 5,
+            
+            })
+            expect(
+                strategy.calculatePrice(7)
+            ).toEqual(
+                // should be 4 times the price (for the group by) + 2 times for the remainder
+                currentAdPrice * 4 +  currentAdPrice * 2
+            )
+        })
+    })
+})
+
--- src/PricingStrategy/index.ts
+++ src/PricingStrategy/index.ts
@@ -1,10 +1,87 @@
+import { AdNameEnum, AllAds, type AllAdsType} from "../Ads"
+
+type CalculatePriceInput = {
+    groupBuyLowerLimit?: number
+    groupBuyUpperLimit?: number
+    fixedDiscountAmount?: number
+    // totalNumberOfAds?: number
+}
+
 // Other pricing strategies can extend this class as they will have some things in common
-abstract class PricingStragyBase {
-    constructor(){
-        
+// the base class will store information about the ads, as well as the ad that the pricing strategy is targetted for (currentAd)
+abstract class PricingStrategyBase {
+    
+    abstract calculatePrice(totalNumberOfAds: number): number;
+
+    ads: AllAdsType
+    currentAd: AdNameEnum
+    calculatePriceInput: CalculatePriceInput
+    
+    constructor(adName: AdNameEnum, calculatePriceInput: CalculatePriceInput){
+        this.ads = AllAds
+        this.currentAd = adName
+        this.calculatePriceInput = calculatePriceInput
+    }
+
+}
+
+// if a user purchases x amount they get a discount, assuming that these discounts can stack up
+// for example if the strategy is buy 3 for 2, if they buy 6, they will get 4
+class GroupBuyPricingStrategy extends PricingStrategyBase {
+
+    calculatePrice(totalNumberOfAds: number): number {
+        const { groupBuyLowerLimit, groupBuyUpperLimit} = this.calculatePriceInput;
+        // validation of inputs
+        if (typeof totalNumberOfAds !== "number" || 
+            typeof groupBuyLowerLimit !== "number" || 
+            typeof groupBuyUpperLimit !== "number") throw new Error("groupBuyLowerLimit, groupBuyUpperLimit and totalNumberOfAds must be of type number")
+
+        if (totalNumberOfAds <1) throw new Error("totalNumberOfAds must be greater than 0")
+        // '2 for 1' is the minimmum allowed input for this strategy, for the sake of this exercise there is no upper limit and it is flexible
+        if (groupBuyUpperLimit < 2) throw new Error("the groupBuyUpperLimit must be at least 2")
+        if (groupBuyLowerLimit < 1) throw new Error("the lower limit must be at least 1")
+            
+        if (groupBuyUpperLimit <= groupBuyLowerLimit) throw new Error("the upper limit must be greater than the lower limit")
+
+        // calculate the total price for this ad
+        const currentAdPrice = this.ads[this.currentAd].retailPriceCentsAUD
+        const numberOfGroups = Math.floor(totalNumberOfAds / groupBuyUpperLimit)
+        const remainderNumberOfAds = totalNumberOfAds % groupBuyUpperLimit
+        const totalPrice = (numberOfGroups * (currentAdPrice * groupBuyLowerLimit)) + (remainderNumberOfAds * currentAdPrice) 
+        return totalPrice
+
     }
 }
 
+// if a user buys any amount of a particular ad, they will get a fixed dollar amount discount
+class FixedDiscountPricingStrategy extends PricingStrategyBase {
+
+    calculatePrice(totalNumberOfAds: number): number {
+        const {fixedDiscountAmount} = this.calculatePriceInput;
+
+        // validation of inputs
+        if (typeof fixedDiscountAmount !== "number" || typeof totalNumberOfAds !== "number") {
+            throw new  Error("fixedDiscountAmount and totalNumberOfAds must be of type number")
+        } 
+
+        if (fixedDiscountAmount <=0) {
+            throw new Error("fixedDiscountAmount must be a positive number")
+        }
+
+        // calculate the total price for this ad
+        const currentAdPrice = this.ads[this.currentAd].retailPriceCentsAUD
+
+        // the discount cant be higher than the value of the ad itself
+        if (fixedDiscountAmount > currentAdPrice) throw new Error(`fixedDiscountAmount must be less than the value of the ad: ${this.currentAd}`)
+            
+        const totalPrice = totalNumberOfAds * (currentAdPrice - fixedDiscountAmount)
+
+        return totalPrice
+}
+}
+
 export {
-    PricingStragyBase
+    PricingStrategyBase,
+    GroupBuyPricingStrategy,
+    FixedDiscountPricingStrategy
 }
