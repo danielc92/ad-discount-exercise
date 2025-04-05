2e8372e1c335a06833ba2d8854ce0672958102e2
daniel corcoran <danielc.92@outlook.com>
2025-04-05 14:40:46 +1100

    feat: Checkout logic and tests

--- src/Checkout/__tests__/index.spec.ts
+++ src/Checkout/__tests__/index.spec.ts
@@ -1,1 +1,120 @@
-describe("Checkout test suite", () => {})
+/*
+    critical paths to test;
+    1. SecondBite
+    - Gets a 3 for 2 deal on Classic Ads
+    2. Axil Coffee Roasters
+    - Gets a discount on Stand out Ads where the price drops to $299.99 per ad
+    3. MYER
+    - Gets a 5 for 4 deal on Stand out Ads
+    - Gets a discount on Premium Ads where the price drops to $389.99 per ad
+*/
+
+import { Checkout } from ".."
+import { AdNameEnum } from "../../Ads"
+import { FixedDiscountPricingStrategy, GroupBuyPricingStrategy } from "../../PricingStrategy"
+
+
+describe("Checkout test suite", () => {
+    it("SecondBite: Checkout with 3 for 2 deal on classic ads", () => {
+        const checkout = new Checkout({
+            CLASSIC: new GroupBuyPricingStrategy(AdNameEnum.CLASSIC, {groupBuyUpperLimit: 3, groupBuyLowerLimit: 2}),
+            PREMIUM: null,
+            STAND_OUT: null
+        })
+
+        checkout.addItem(AdNameEnum.CLASSIC)
+        checkout.addItem(AdNameEnum.CLASSIC)
+        checkout.addItem(AdNameEnum.CLASSIC)
+
+        const formattedPrice = checkout.showPrice()
+
+        expect(formattedPrice).toEqual("$539.98")
+    })
+
+    it("Axil Coffee Roasters: they get 23$ (2300 cents) off all standout ads ", () => {
+        const checkout = new Checkout({
+            CLASSIC: null,
+            PREMIUM: null,
+            STAND_OUT: new FixedDiscountPricingStrategy(AdNameEnum.STAND_OUT, {fixedDiscountAmount: 2300})
+        })
+
+        // we will buy two to keep things simple
+        checkout.addItem(AdNameEnum.STAND_OUT)
+        checkout.addItem(AdNameEnum.STAND_OUT)
+
+        const formattedPrice = checkout.showPrice()
+
+        expect(formattedPrice).toEqual("$599.98")
+    })
+
+    it("MYER: 5 for 4 on stand out ads + 5$ discount on all premium ads", () => {
+        const checkout = new Checkout({
+            CLASSIC: null,
+            PREMIUM: new FixedDiscountPricingStrategy(AdNameEnum.PREMIUM, {fixedDiscountAmount: 500}),
+            STAND_OUT: new GroupBuyPricingStrategy(AdNameEnum.STAND_OUT, {groupBuyUpperLimit: 5, groupBuyLowerLimit: 4})
+        })
+
+        checkout.addItem(AdNameEnum.PREMIUM)
+        checkout.addItem(AdNameEnum.STAND_OUT)
+        checkout.addItem(AdNameEnum.STAND_OUT)
+        checkout.addItem(AdNameEnum.STAND_OUT)
+        checkout.addItem(AdNameEnum.STAND_OUT)
+        checkout.addItem(AdNameEnum.STAND_OUT)
+
+        const formattedPrice = checkout.showPrice()
+
+        expect(formattedPrice).toEqual("$1681.95")
+    })
+
+    // other test cases
+
+    it("2 of each ad type without any strategies",  () => {
+
+        const checkout = new Checkout()
+
+        checkout.addItem(AdNameEnum.PREMIUM)
+        checkout.addItem(AdNameEnum.PREMIUM)
+        checkout.addItem(AdNameEnum.STAND_OUT)
+        checkout.addItem(AdNameEnum.STAND_OUT)
+        checkout.addItem(AdNameEnum.CLASSIC)
+        checkout.addItem(AdNameEnum.CLASSIC)
+
+        const formattedPrice = checkout.showPrice()
+
+        expect(formattedPrice).toEqual("$1975.94")
+    })
+
+    it("1 premium without any strategies",  () => {
+
+        const checkout = new Checkout()
+
+        checkout.addItem(AdNameEnum.PREMIUM)
+
+        const formattedPrice = checkout.showPrice()
+
+        expect(formattedPrice).toEqual("$394.99")
+    })
+
+    it("1 classic without any strategies",  () => {
+
+        const checkout = new Checkout()
+
+        checkout.addItem(AdNameEnum.CLASSIC)
+
+        const formattedPrice = checkout.showPrice()
+
+        expect(formattedPrice).toEqual("$269.99")
+    })
+
+    it("1 standout without any strategies",  () => {
+
+        const checkout = new Checkout()
+
+        checkout.addItem(AdNameEnum.STAND_OUT)
+
+        const formattedPrice = checkout.showPrice()
+
+        expect(formattedPrice).toEqual("$322.99")
+    })
+
+})
--- src/Checkout/index.ts
+++ src/Checkout/index.ts
@@ -1,6 +1,79 @@
-class Checkout {
-    constructor(){
+import { AdNameEnum, AllAds } from "../Ads"
+import { FixedDiscountPricingStrategy, GroupBuyPricingStrategy, PricingStrategyBase } from "../PricingStrategy"
 
+
+type PricingStrategyOrNull = GroupBuyPricingStrategy | FixedDiscountPricingStrategy | null
+
+// when you construct a Checkout instance you can specify a pricing strategy for each ad type
+// if there is no strategy you wish to use, then you may leave as null
+type CheckoutConstructorOptions = {
+    [key in AdNameEnum]: PricingStrategyOrNull
+}
+
+// the default if not supplied will be no stragey for each ad type
+const DEFAULT_PRICING_STRATEGY = {
+    CLASSIC: null,
+    PREMIUM: null,
+    STAND_OUT:null
+}
+
+class Checkout {
+
+    // number of each ad for this checkout
+    adCounts: {
+        [key in AdNameEnum]: number
+    }
+
+    pricingStrategies: {
+        [key in AdNameEnum]: GroupBuyPricingStrategy | FixedDiscountPricingStrategy | null
+    }
+
+    totalPriceInCents: number
+
+    constructor(pricingStrategies?: CheckoutConstructorOptions){
+        // initialise with 0 for each ad
+        this.adCounts = {
+            CLASSIC: 0,
+            PREMIUM: 0,
+            STAND_OUT: 0
+        }
+        this.pricingStrategies = pricingStrategies ?? DEFAULT_PRICING_STRATEGY
+        this.totalPriceInCents = 0
+    }
+
+    // increment the ad count for a particular ad name
+    // this uses the builder pattern, such that multiple calls can be used in a row
+    addItem = (adName: AdNameEnum) => {
+        this.adCounts[adName] += 1
+        return this
+    }
+
+    // calculates the price total without any formatting
+    // the calculation pathway is simplified here as you either have a pricing strategy 
+    // (something that extends the PricingStrategyBase abstract class) or you use the default one
+    calculatePriceInCents = () => {
+
+        let total = 0
+
+        for (const adName of Object.values(AdNameEnum)) {
+            if (this.pricingStrategies[adName] instanceof PricingStrategyBase) {
+                const subtotal = this.pricingStrategies[adName].calculatePrice(this.adCounts[adName])
+                total += subtotal
+            } 
+            else {
+                // default pricing strategy is to use the original prices multiplied by the count of ads
+                total += (AllAds[adName].retailPriceCentsAUD * this.adCounts[adName])
+            }
+        }
+
+        this.totalPriceInCents = total
+
+    }
+
+    // calculates the cents price and displays it in a user friendly manner
+    showPrice = (): string => {
+        this.calculatePriceInCents()
+        return `$${this.totalPriceInCents/100}`
     }
 }
 
